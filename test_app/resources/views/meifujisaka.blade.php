5a0cf62ceaf8c3631b6618463e298962746ae3ea
Meifujisaka <74054713+Meifujisaka@users.noreply.github.com>
2020-12-12 11:05:40 +0900

    addintroduce

---
+++ test_app/resources/views/meifujisaka.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>Mei Fujisaka
+
+    </head>
+    <body><br>
+      I'm 24 years old. Autually just turned 24 two weeks ago.
+      I'm living in Suita city,Osaka.I've been living here year to date.</br>
+    <br>Nice to meet you.</br>
+
+
+
+    </body>
+</html>
